74fef77cfb88e9d3961258cf4130f29c26b94d97
kodicat <yevhenii.yukhymchuk@gmail.com>
2024-10-27 17:06:05 +0200

    Implement read, write, query operations

--- client/run.py
+++ client/run.py
@@ -1,221 +1,4 @@
-# import azure.cosmos.documents as documents
-
-# import datetime
-
-# import config
-
-
-# ----------------------------------------------------------------------------------------------------------
-# Prerequistes -
-#
-# 1. An Azure Cosmos account -
-#    https://docs.microsoft.com/azure/cosmos-db/create-cosmosdb-resources-portal#create-an-azure-cosmos-db-account
-#
-# 2. Microsoft Azure Cosmos PyPi package -
-#    https://pypi.python.org/pypi/azure-cosmos/
-# ----------------------------------------------------------------------------------------------------------
-# Sample - demonstrates the basic CRUD operations on a Item resource for Azure Cosmos
-# ----------------------------------------------------------------------------------------------------------
-
-# HOST = config.settings['host']
-# MASTER_KEY = config.settings['master_key']
-# DATABASE_ID = config.settings['database_id']
-# CONTAINER_ID = config.settings['container_id']
-
-
-# def create_items(container):
-#     print('\nCreating Items\n')
-
-#     # Create a SalesOrder object. This object has nested properties and various types including numbers, DateTimes and strings.
-#     # This can be saved as JSON as is without converting into rows/columns.
-#     sales_order = get_sales_order("SalesOrder1")
-#     container.create_item(body=sales_order)
-
-#     # As your app evolves, let's say your object has a new schema. You can insert SalesOrderV2 objects without any
-#     # changes to the database tier.
-#     sales_order2 = get_sales_order_v2("SalesOrder2")
-#     container.create_item(body=sales_order2)
-
-# def scale_container(container):
-#     print('\nScaling Container\n')
-    
-#     # You can scale the throughput (RU/s) of your container up and down to meet the needs of the workload. Learn more: https://aka.ms/cosmos-request-units
-#     try:
-#         offer = container.read_offer()
-#         print('Found Offer and its throughput is \'{0}\''.format(offer.offer_throughput))
-
-#         offer.offer_throughput += 100
-#         container.replace_throughput(offer.offer_throughput)
-
-#         print('Replaced Offer. Offer Throughput is now \'{0}\''.format(offer.offer_throughput))
-    
-#     except exceptions.CosmosHttpResponseError as e:
-#         if e.status_code == 400:
-#             print('Cannot read container throuthput.');
-#             print(e.http_error_message);
-#         else:
-#             raise;
-
-# def read_item(container, doc_id, account_number):
-#     print('\nReading Item by Id\n')
-
-#     # We can do an efficient point read lookup on partition key and id
-#     response = container.read_item(item=doc_id, partition_key=account_number)
-
-#     print('Item read by Id {0}'.format(doc_id))
-#     print('Partition Key: {0}'.format(response.get('partitionKey')))
-#     print('Subtotal: {0}'.format(response.get('subtotal')))
-
-
-# def read_items(container):
-#     print('\nReading all items in a container\n')
-
-#     # NOTE: Use MaxItemCount on Options to control how many items come back per trip to the server
-#     #       Important to handle throttles whenever you are doing operations such as this that might
-#     #       result in a 429 (throttled request)
-#     item_list = list(container.read_all_items(max_item_count=10))
-
-#     print('Found {0} items'.format(item_list.__len__()))
-
-#     for doc in item_list:
-#         print('Item Id: {0}'.format(doc.get('id')))
-
-
-# def query_items(container, account_number):
-#     print('\nQuerying for an  Item by Partition Key\n')
-
-#     # Including the partition key value of account_number in the WHERE filter results in a more efficient query
-#     items = list(container.query_items(
-#         query="SELECT * FROM r WHERE r.partitionKey=@account_number",
-#         parameters=[
-#             { "name":"@account_number", "value": account_number }
-#         ]
-#     ))
-
-#     print('Item queried by Partition Key {0}'.format(items[0].get("id")))
-
-
-# def replace_item(container, doc_id, account_number):
-#     print('\nReplace an Item\n')
-
-#     read_item = container.read_item(item=doc_id, partition_key=account_number)
-#     read_item['subtotal'] = read_item['subtotal'] + 1
-#     response = container.replace_item(item=read_item, body=read_item)
-
-#     print('Replaced Item\'s Id is {0}, new subtotal={1}'.format(response['id'], response['subtotal']))
-
-
-# def upsert_item(container, doc_id, account_number):
-#     print('\nUpserting an item\n')
-
-#     read_item = container.read_item(item=doc_id, partition_key=account_number)
-#     read_item['subtotal'] = read_item['subtotal'] + 1
-#     response = container.upsert_item(body=read_item)
-
-#     print('Upserted Item\'s Id is {0}, new subtotal={1}'.format(response['id'], response['subtotal']))
-
-
-# def delete_item(container, doc_id, account_number):
-#     print('\nDeleting Item by Id\n')
-
-#     response = container.delete_item(item=doc_id, partition_key=account_number)
-
-#     print('Deleted item\'s Id is {0}'.format(doc_id))
-
-
-# def get_sales_order(item_id):
-#     order1 = {'id' : item_id,
-#             'partitionKey' : 'Account1',
-#             'purchase_order_number' : 'PO18009186470',
-#             'order_date' : datetime.date(2005,1,10).strftime('%c'),
-#             'subtotal' : 419.4589,
-#             'tax_amount' : 12.5838,
-#             'freight' : 472.3108,
-#             'total_due' : 985.018,
-#             'items' : [
-#                 {'order_qty' : 1,
-#                     'product_id' : 100,
-#                     'unit_price' : 418.4589,
-#                     'line_price' : 418.4589
-#                 }
-#                 ],
-#             'ttl' : 60 * 60 * 24 * 30
-#             }
-
-#     return order1
-
-
-# def get_sales_order_v2(item_id):
-#     # notice new fields have been added to the sales order
-#     order2 = {'id' : item_id,
-#             'partitionKey' : 'Account2',
-#             'purchase_order_number' : 'PO15428132599',
-#             'order_date' : datetime.date(2005,7,11).strftime('%c'),
-#             'due_date' : datetime.date(2005,7,21).strftime('%c'),
-#             'shipped_date' : datetime.date(2005,7,15).strftime('%c'),
-#             'subtotal' : 6107.0820,
-#             'tax_amount' : 586.1203,
-#             'freight' : 183.1626,
-#             'discount_amt' : 1982.872,
-#             'total_due' : 4893.3929,
-#             'items' : [
-#                 {'order_qty' : 3,
-#                     'product_code' : 'A-123',      # notice how in item details we no longer reference a ProductId
-#                     'product_name' : 'Product 1',  # instead we have decided to denormalise our schema and include
-#                     'currency_symbol' : '$',       # the Product details relevant to the Order on to the Order directly
-#                     'currency_code' : 'USD',       # this is a typical refactor that happens in the course of an application
-#                     'unit_price' : 17.1,           # that would have previously required schema changes and data migrations etc.
-#                     'line_price' : 5.7
-#                 }
-#                 ],
-#             'ttl' : 60 * 60 * 24 * 30
-#             }
-
-#     return order2
-
-# def run_sample():
-#     client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
-#     try:
-#         # setup database for this sample
-#         try:
-#             db = client.create_database(id=DATABASE_ID)
-#             print('Database with id \'{0}\' created'.format(DATABASE_ID))
-
-#         except exceptions.CosmosResourceExistsError:
-#             db = client.get_database_client(DATABASE_ID)
-#             print('Database with id \'{0}\' was found'.format(DATABASE_ID))
-
-#         # setup container for this sample
-#         try:
-#             container = db.create_container(id=CONTAINER_ID, partition_key=PartitionKey(path='/partitionKey'))
-#             print('Container with id \'{0}\' created'.format(CONTAINER_ID))
-
-#         except exceptions.CosmosResourceExistsError:
-#             container = db.get_container_client(CONTAINER_ID)
-#             print('Container with id \'{0}\' was found'.format(CONTAINER_ID))
-
-#         scale_container(container)
-#         create_items(container)
-#         read_item(container, 'SalesOrder1', 'Account1')
-#         read_items(container)
-#         query_items(container, 'Account1')
-#         replace_item(container, 'SalesOrder1', 'Account1')
-#         upsert_item(container, 'SalesOrder1', 'Account1')
-#         delete_item(container, 'SalesOrder1', 'Account1')
-
-#         # cleanup database after sample
-#         try:
-#             client.delete_database(db)
-
-#         except exceptions.CosmosResourceNotFoundError:
-#             pass
-
-#     except exceptions.CosmosHttpResponseError as e:
-#         print('\nrun_sample has caught an error. {0}'.format(e.message))
-
-#     finally:
-#             print("\nrun_sample done")
-
+import azure.cosmos.documents as doc
 import azure.cosmos.exceptions as exceptions
 import urllib3
 import config
@@ -223,6 +6,10 @@ import config
 from azure.cosmos.cosmos_client import CosmosClient
 from azure.cosmos.partition_key import PartitionKey
 
+# Initialize the Cosmos client
+connection_policy = doc.ConnectionPolicy()
+# Disable in production
+connection_policy.DisableSSLVerification = "true"
 # disable ssl certificate warnings
 urllib3.disable_warnings()
 
@@ -230,41 +17,61 @@ HOST = config.settings['host']
 KEY = config.settings['key']
 DATABASE_ID = config.settings['database_id']
 
-# create item for cosmos db depending on the task
-def createItem(i):
-    id = i + 1
-    id_str = str(id)
-    tenantId = str(i // 100 + 1 )
-    obj = {
-        'tenantId': tenantId,
-        'id': id_str,
-        'productName': 'Widget' + id_str,
-        'productModel': 'Model' + id_str
-    }
-    return obj
-
-def createItemsInRange(max_index):
-    ids = range(0, max_index)
-    return list(map(lambda i: createItem(i), ids))
-
 def getOrCreateDatabase(client):
     try:
-        database = client.create_database(id=DATABASE_ID)
+        database = client.create_database(id=DATABASE_ID, offer_throughput=20000)
         print('Database with id \'{0}\' created'.format(DATABASE_ID))
     except exceptions.CosmosResourceExistsError:
         database = client.get_database_client(DATABASE_ID)
         print('Database with id \'{0}\' was found'.format(DATABASE_ID))
     return database
 
-def createContainer(database, container_name, partition_key):
+def getOrCreateContainer(database, container_name, partition_key, unique_key_policy=None):
     container = None
     try:
-        container = database.create_container(id=container_name, partition_key=partition_key)
+        if (unique_key_policy == None):
+            container = database.create_container(
+                id=container_name,
+                partition_key=partition_key,
+                offer_throughput=5000,
+            )
+        else:
+            container = database.create_container(
+                id=container_name,
+                partition_key=partition_key,
+                offer_throughput=5000,
+                unique_key_policy=unique_key_policy
+            )
         print('Container with id \'{0}\' created'.format(container_name))
     except exceptions.CosmosResourceExistsError:
         container = database.get_container_client(container_name)
         print('Container with id \'{0}\' was found'.format(container_name))
     return container
+
+def insert_items(container, items):
+    for x in items:
+        container.upsert_item(body=x)
+    printLastRequestStatistics(container, 'Create item with ' + str(len(items)) + ' items')
+
+def read_item(container, name, model):
+    items = container.query_items(
+        query='SELECT * FROM c WHERE c.productName = @name AND c.productModel = @model',
+        parameters=[{'name': '@name', 'value': name}, {'name': '@model', 'value': model}],
+        enable_cross_partition_query=True
+    )
+    dummy = [item for item in items]
+    printLastRequestStatistics(container, 'Query item by model: ' + model + ' and name: ' + name)
+
+def read_all_items(container):
+    item_list = list(container.read_all_items(max_item_count=1000))
+    printLastRequestStatistics(container, 'Read all items with: ' + str(len(item_list)) + ' items')
+
+def printLastRequestStatistics(container, text):
+    headers = container.client_connection.last_response_headers
+    print('  Operation: ' + text)
+    print('    Duration ms: ', headers['x-ms-request-duration-ms'])
+    print('    Rate Units (RU): ', headers['x-ms-request-charge'])
+    print()
 
 def cleanUpDatabase(client, database):
     try:
@@ -272,38 +79,56 @@ def cleanUpDatabase(client, database):
     except exceptions.CosmosResourceNotFoundError:
         pass
 
-def upsert_items(container, items):
-    for item in items:
-        container.upsert_item(item)
+def create_items(count):
+    ids = range(1, count + 1)
+    return list(map(lambda i: create_item(i), ids))
+
+def create_item(i):
+    id = str(i)
+    return {
+        'id': id,
+        'productName': 'Widget' + id,
+        'productModel': 'Model' + id
+    }
+
+def run_container(container, items):
+    print_container_statistics(container)
+    insert_items(container, items)
+    read_item(container, 'Widget99', 'Model99')
+    read_all_items(container)
+    print('----')
+
+def print_container_statistics(container):
+    container_props = container.read()
+    print('Container id: ', container_props.get('id'), ', partitionKey: ', container_props.get('partitionKey'), ', uniqueKeyPolicy: ', container_props.get('uniqueKeyPolicy'))
 
 def run():
     try:
-        client = CosmosClient(url=HOST, credential=KEY, connection_verify=False)
+        client = CosmosClient(url=HOST, credential=KEY, connection_verify=False, connection_policy=connection_policy)
         database = getOrCreateDatabase(client)
 
         partition_key1 = PartitionKey(path='/id')
-        container_1_100 = createContainer(database, container_name="container_1_100", partition_key=partition_key1)
-        container_1_1000 = createContainer(database, container_name="container_1_1000", partition_key=partition_key1)
+        container_1_100 = getOrCreateContainer(database, container_name="container_1_100", partition_key=partition_key1)
+        container_1_1000 = getOrCreateContainer(database, container_name="container_1_1000", partition_key=partition_key1)
 
-        partition_key2 = PartitionKey(path=['/tenantId', '/id'], kind='MultiHash')
-        container_2_100 = createContainer(database, container_name="container_2_100", partition_key=partition_key2)
-        container_2_1000 = createContainer(database, container_name="container_2_1000", partition_key=partition_key2)
+        partition_key2 = PartitionKey(path=['/productName', '/productModel'], kind='MultiHash')
+        uniqueKeyPolicy = {'uniqueKeys': [{'paths': ['/productName', '/productModel']}]}
+        container_2_100 = getOrCreateContainer(database, container_name="container_2_100", partition_key=partition_key2, unique_key_policy=uniqueKeyPolicy)
+        container_2_1000 = getOrCreateContainer(database, container_name="container_2_1000", partition_key=partition_key2, unique_key_policy=uniqueKeyPolicy)
 
-        items_100 = createItemsInRange(100)
-        items_1000 = createItemsInRange(1000)
+        items_100 = create_items(count=100)
+        items_1000 = create_items(count=1000)
 
-        upsert_items(container_1_100, items_100)
-        upsert_items(container_1_1000, items_1000)
-        upsert_items(container_2_100, items_100)
-        upsert_items(container_2_1000, items_1000)
-
-        # check statistics???
+        run_container(container_1_100, items_100)
+        run_container(container_1_1000, items_1000)
+        run_container(container_2_100, items_100)
+        run_container(container_2_1000, items_1000)
 
     except exceptions.CosmosHttpResponseError as e:
         print('\n----Error. {0}'.format(e.message))
 
     finally:
-            print("\n run() is done")
+        print("\n run() is done")
 
     # cleanUpDatabase(client, database)
 
